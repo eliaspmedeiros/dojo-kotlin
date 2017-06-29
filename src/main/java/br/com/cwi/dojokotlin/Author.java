6c0feee5fe3295eeaedc84416bea7c9bbe827776
elias.medeiros <elias.medeiros@cwi.com.br>
2017-06-29 14:38:08 -0300

    improve java example

--- src/main/java/br/com/cwi/dojokotlin/Author.java
+++ src/main/java/br/com/cwi/dojokotlin/Author.java
@@ -1,19 +1,43 @@
 package br.com.cwi.dojokotlin;
 
+import java.util.Date;
+
 public class Author {
 
     private String name;
+    private String gender;
+    private Date birth;
+    private Date death;
+    private Boolean flag;
 
-    public Author(String name) {
+    public Author(String name, String gender, Date birth, Date death) {
         this.name = name;
+        this.gender = gender;
+        this.birth = birth;
+        this.death = death;
     }
 
     public String getName() {
         return name;
     }
 
-    public void setName(String name) {
-        this.name = name;
+    public String getGender() {
+        return gender;
     }
 
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public Date getBirth() {
+        return birth;
+    }
+
+    public Date getDeath() {
+        return death;
+    }
+
+    public void setDeath(Date death) {
+        this.death = death;
+    }
 }
