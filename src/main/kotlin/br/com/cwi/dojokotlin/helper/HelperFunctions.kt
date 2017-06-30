3764641f0a85aba7588d4cd5c65f401034519c4d
Elias Medeiros <eliaspmedeiros@gmail.com>
2017-06-30 14:20:34 -0300

    VI

--- src/main/kotlin/br/com/cwi/dojokotlin/helper/HelperFunctions.kt
+++ src/main/kotlin/br/com/cwi/dojokotlin/helper/HelperFunctions.kt
@@ -1,3 +1,5 @@
 package br.com.cwi.dojokotlin.helper
 
 fun failure() = println("falha")
+
+fun getLoggedUser(): String? = "Moisés"
--- src/main/kotlin/br/com/cwi/dojokotlin/store/BaseStore.kt
+++ src/main/kotlin/br/com/cwi/dojokotlin/store/BaseStore.kt
@@ -1,9 +1,22 @@
 package br.com.cwi.dojokotlin.store
 
+import br.com.cwi.dojokotlin.helper.getLoggedUser
+
 abstract class BaseStore<T>(open var items: HashMap<T, Int>) {
+
+    val user: String? by lazy {
+        println("Accessing current user")
+        getLoggedUser()
+    }
 
     fun sellItem(itemToSell: T, success: () -> Unit, failure: () -> Unit) {
         val stockItem = items[itemToSell]
+
+        if (user != null) {
+            println("Realizando compra com usuário: $user")
+        } else {
+            failure()
+        }
 
         if (stockItem != null && stockItem > 0) {
             items[itemToSell] = items[itemToSell]!! - 1
